e41d207985bfed2fff85c865cf43e5a7ef03f2c0
oudarja <oudarja360@gmail.com>
2025-02-09 17:22:15 +0600

    moment package installed

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,44 +1,38 @@
-import React from 'react'
-import Layout from './../../components/Layout/Layout';
-import AdminMenu from '../../components/Layout/AdminMenu';
+import React from "react";
+import Layout from "./../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
 
-import { useAuth } from '../../context/auth';
+import { useAuth } from "../../context/auth";
 
 const Admindashboard = () => {
-  
   const [auth] = useAuth();
 
   return (
     <Layout title={"Admin dashboard"}>
-    {/* m-3: Adds margin of 3 units (Bootstrap's spacing scale) on all
+      {/* m-3: Adds margin of 3 units (Bootstrap's spacing scale) on all
      sides: top, right, bottom, and left. 
      p-3: Adds padding of 3 units on all sides: top, right, bottom, and
      left. 
      */}
-     <div className="container-fluid m-3 p-3">
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
 
-     <div className="row">
-
-      <div className="col-md-3">
-             <AdminMenu/>
-      </div>
-
-  {/* Takes 9 out of 12 columns (75% width) when the screen
+          {/* Takes 9 out of 12 columns (75% width) when the screen
    size is medium or larger. */}
-      <div className="col-md-9">
+          <div className="col-md-9">
             <div className="card w-75 p-3">
-
-            <h3>Admin Name: {auth?.user?.name}</h3>
-            <h3>Admin Email: {auth?.user?.email}</h3>
-            <h3>Admin Contact: {auth?.user?.phone}</h3>
+              <h3>Admin Name: {auth?.user?.name}</h3>
+              <h3>Admin Email: {auth?.user?.email}</h3>
+              <h3>Admin Contact: {auth?.user?.phone}</h3>
             </div>
+          </div>
+        </div>
       </div>
-     </div>
-     
-     
-     </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Admindashboard
+export default Admindashboard;
