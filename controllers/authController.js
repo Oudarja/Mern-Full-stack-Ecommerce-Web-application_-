5fd7c0b8f5cf3cb86283cbc143c18b25c3bc57dc
Oudarja Barman Tanmoy <51699905+Oudarja@users.noreply.github.com>
2025-02-09 14:34:09 +0600

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "./helpers/authHelper.js";
 import userModel from "../models/userModel.js";
 import JWT from "jsonwebtoken";
 
